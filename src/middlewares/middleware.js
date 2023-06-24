5ff712f521fbd9e6bb00aed3ac25512b747cff96
murillo <murillo1047@gmail.com>
2023-06-24 09:53:28 -0300

    criar contato criado

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -1,6 +1,7 @@
 exports.middlewareGlobal = (req , res , next) => {
     res.locals.errors = req.flash('errors');
     res.locals.success = req.flash('success');
+    res.locals.user = req.session.user;
     next();
 };
 
@@ -20,3 +21,13 @@ exports.csrfMiddleware = (req, res, next) => {
     res.locals.csrfToken = req.csrfToken();
     next();
 }
+
+exports.loginRequired = (res, req, next) => {
+    if(!req.session.user){
+        req.flash('errors', 'Você precisa fazer login.')
+        req.session.save(() => res.redirect('/'));
+        return;
+    }
+
+    next();
+};
