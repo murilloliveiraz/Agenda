d77b3381c59241a5fea6455369f00a8771d4e506
murillo <murillo1047@gmail.com>
2023-06-23 21:31:24 -0300

    tela de login, rotas de login e criacao de conta feitos

--- routes.js
+++ routes.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const route = express.Router();
 const homeController = require('./src/controllers/homeController');
-const contatoController = require('./src/controllers/contatoController')
-
+const loginController = require('./src/controllers/loginController');
 
 // Rotas da Home
-route.get('/', homeController.paginaInicial);
-route.post('/', homeController.trataPost);
+route.get('/', homeController.index);
 
-// rotas de Contato
-route.get('/contato', contatoController.paginaInicial);
+//rotas de login
+route.get('/login/index', loginController.index);
+route.post('/login/register', loginController.register);
 
 module.exports = route;
--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -1,5 +1,6 @@
 exports.middlewareGlobal = (req , res , next) => {
-    res.locals.umaVariavelLocal = 'Este é o valor da variavel local'
+    res.locals.errors = req.flash('errors');
+    res.locals.success = req.flash('success');
     next();
 };
 
@@ -8,9 +9,11 @@ exports.outroMiddleware = (req , res , next) => {
 };
 
 exports.checkCsrfError = (err, req, res, next) => {
-    if(err && 'EBADCSRFTOKEN' === err.code){
+    if(err){
         return res.render('404')
     }
+
+    next();
 };
 
 exports.csrfMiddleware = (req, res, next) => {
